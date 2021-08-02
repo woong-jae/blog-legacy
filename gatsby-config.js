86635c6632ca3fd00aaed7f951a0cacb698ed789
woong-jae <jwchung0828@gmail.com>
2021-08-02 22:27:03 +0900

    favicon, manifest 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://woong-jae.netlify.app",
     title: "햣 블로그",
-    description: "기술 블로그",
+    description: "woong-jae의 기술 블로그",
     author: "woong-jae",
     socials: {
       email: "jwchung0828@gmail.com",
@@ -53,6 +53,24 @@ module.exports = {
             },
           },
         ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "햣 블로그 by woong-jae",
+        short_name: "햣 블로그",
+        start_url: "/",
+        background_color: "##25292e",
+        theme_color: "##25292e",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
       },
     },
     // {
