9d432bdfea19bb83d37804babd688e2c5ab5b301
woong-jae <jwchung0828@gmail.com>
2021-08-02 20:11:35 +0900

    config 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,58 +40,6 @@ module.exports = {
           path: `${__dirname}/content/blog`
       }
     },  
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map((edge) => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                });
-              });
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields { slug }
-                      frontmatter {
-                        title
-                        date
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml",
-            title: "woong-jae RSS feed",
-          },
-        ],
-      },
-    },
     `gatsby-plugin-mdx`,
     // {
     //   resolve: "gatsby-plugin-google-analytics",
