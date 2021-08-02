29cb1c24767d4d8c5463ac2143ce6a6e11478cc8
woong-jae <jwchung0828@gmail.com>
2021-08-03 03:08:13 +0900

    구글 analytics 연결

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,11 +37,11 @@ module.exports = {
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
