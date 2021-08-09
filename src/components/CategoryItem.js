bf1d637edd0571db14ecfcb8a5b5b0a7f0a65400
woong-jae <jwchung0828@gmail.com>
2021-08-09 14:01:44 +0900

    카테고리 디자인 개선 2

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -5,11 +5,13 @@ import styled from "styled-components"
 const CategoryContainer = styled.div`
     background-color: ${props => props.theme.colors.blackLight};
     border-radius: 30px;
-    margin: 0 4px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+    margin: 0 5px;
     color: ${props => props.theme.colors.whiteSmoke};
     &:hover {
-      background: ${props => props.theme.colors.gradient};
-      color: ${props => props.theme.colors.blackLight};
+        transform: rotate(10deg);
+        background: ${props => props.theme.colors.gradient};
+        color: ${props => props.theme.colors.blackLight};
     }
     a {
         color: inherit;
